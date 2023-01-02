baee7f3f3739362c682fa0ecacd1d18b33f9edb6
mahmood2122 <mahmoodsino40@gmail.com>
2023-01-02 10:56:28 +0300

    fix color in products card

--- components/layout/details/sections/RelatedProducts.tsx
+++ components/layout/details/sections/RelatedProducts.tsx
@@ -5,7 +5,7 @@ const RelatedProducts = () => {
   return (
     <div className="mt-10 text-center">
       <h3 className="text-2xl font-semibold">RELATED PRODUCTS</h3>
-      <div className="grid lg:grid-cols-5 md:grid-cols-3 mt-5 sm:flex sm:flex-col sm:items-center">
+      <div className="md:grid lg:grid-cols-5 md:grid-cols-3 mt-5 sm:flex sm:flex-col sm:items-center">
         <RelatedProductsCard />
         <RelatedProductsCard />
         <RelatedProductsCard />
